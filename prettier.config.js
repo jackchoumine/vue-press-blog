88ca9333fe1b197010cc7a2493958bb2f15ddc49
jack <jackzhoumine@gmail.com>
2021-07-31 11:58:48 +0800

    chore: 添加prettier默认配置

--- prettier.config.js
+++ prettier.config.js
@@ -15,11 +15,11 @@ module.exports = {
   proseWrap: 'preserve', // 默认值。因为使用了一些折行敏感型的渲染器（如GitHub comment）而按照markdown文本样式进行折行
   bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
   htmlWhitespaceSensitivity: 'ignore',
-  jsxBracketSameLine: true, // 在jsx中把'>' 是否单独放一行
+  jsxBracketSameLine: true, // 在jsx中把'>' 是否单独放同一行
   jsxSingleQuote: true, // 在jsx中使用单引号代替双引号
   // parser: 'vue-eslint-parser', //  格式化的解析器，默认是babylon
   // trailingComma: 'es5', 'none' "all" // 在对象或数组最后一个元素后面是否加逗号（在ES5中加尾逗号）
-  trailingComma: 'none', // 可能和 eslint 的 comma-dangle 冲突
+  trailingComma: 'es5', // 可能和 eslint 的 comma-dangle 冲突
   // eslintIntegration: false, // 不让prettier使用eslint的代码格式进行校验
   // requireConfig: false, // Require a 'prettierconfig' to format prettier
   // ignorePath: '.prettierignore', // 不使用prettier格式化的文件填写在项目的.prettierignore文件中
@@ -33,8 +33,8 @@ module.exports = {
     {
       files: '*.md',
       options: {
-        tabWidth: 2
-      }
-    }
-  ]
+        tabWidth: 2,
+      },
+    },
+  ],
 }
