b1e97ccdc449f491b16aa14e0d175be7b444755f
jack <jackzhoumine@gmail.com>
2021-11-22 00:49:02 +0800

    使用vue语法操作web component

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,13 +3,14 @@
  * @Hash: ''
  * @Date: 2021-06-01 14:30:02 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-11-21 04:23:30 +0800
+ * @LastEditTime: 2021-11-22 00:47:50 +0800
  * @LastEditors : JackChou
 -->
 <template>
   <div>
-    <span class="rating">rating outside</span>
-    <my-rating ref="myRating" max-value="5" value="3" />
+    <span class="rating">{{ msg }}</span>
+    <my-rating ref="myRating" :max-value="maxValue" :value="value" />
+    <ElButton @click="changeRating">修改评价</ElButton>
     <DynamicComponent />
     <el-button type="primary" @click="showConfirm">显示弹窗</el-button>
     <DebounceTest />
@@ -20,6 +21,13 @@
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      msg: 'Hello web components in stencil!',
+      maxValue: 5,
+      value: 2,
+    }
+  },
   mounted() {
     console.log('Home mounted')
     const myRatingComponent = document.querySelector('my-rating')
@@ -27,6 +35,7 @@ export default {
       // myRatingComponent.value = 4;
       myRatingComponent.setAttribute('value', 4)
       console.log('set value')
+      this.maxValue = 10
       myRatingComponent.getValue({ maxValue: 10, value: 5 }).then(value => {
         console.log('get value', value)
       })
@@ -35,13 +44,16 @@ export default {
 
     myRatingComponent.addEventListener('ratingChange', ({ detail }) => {
       console.log('rating changed', detail)
-      alert(`rating change ${detail.value}`)
+      // alert(`rating change ${detail.value}`)
     })
   },
   methods: {
     showConfirm() {
       // TODO如何知道用户点了确定或者取消？
       this.$myConfirm('删除后不能恢复', '确定删除吗？')
+    },
+    changeRating() {
+      this.value = Math.floor(Math.random() * this.maxValue)
     },
   },
 }
