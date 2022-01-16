18f4c62e6c7ec137287ad7872e8cec4521d51830
jack <jackzhoumine@gmail.com>
2022-01-16 23:32:22 +0800

    构建webcomponent

--- src/views/web-component/components/MyRating.vue
+++ src/views/web-component/components/MyRating.vue
@@ -2,7 +2,7 @@
  * @Description : 
  * @Date        : 2022-01-16 22:01:25 +0800
  * @Author      : JackChou
- * @LastEditTime: 2022-01-16 22:07:59 +0800
+ * @LastEditTime: 2022-01-16 22:54:49 +0800
  * @LastEditors : JackChou
 -->
 <template>
@@ -10,35 +10,59 @@
     <span class="rating">{{ msg }}</span>
     <my-rating
       ref="myRating"
-      :max-value="maxValue"
-      :value="value"
+      :max-value="innerMaxValue"
+      :value="innerValue"
       is-show
       :person.prop="{ name: 'jack' }"
       :personArray.prop="persons"
       @ratingChange="ratingChange"
     />
-    <ElButton @click="changeRating">修改评价</ElButton>
+    <!-- NOTE 单独设置 默认属性 -->
+    <Button :type="type" v-bind="buttonOptions" @click="changeRating">修改评价</Button>
   </div>
 </template>
 
 <script>
+import { Button } from 'element-ui'
 export default {
   name: 'MyVueRating',
+  components: {
+    Button,
+  },
+  props: {
+    maxValue: {
+      type: Number,
+      default: 5,
+    },
+    value: {
+      type: Number,
+      default: 3,
+    },
+    type: {
+      type: String,
+      default: 'primary',
+    },
+    buttonOptions: {
+      type: Object,
+      default: () => ({
+        size: 'small',
+      }),
+    },
+  },
   data() {
     return {
       msg: 'Hello web components in stencil!',
-      maxValue: 5,
-      value: 2,
+      innerValue: this.value,
+      innerMaxValue: this.maxValue,
       persons: [{ name: 'jack', age: 30 }],
     }
   },
   mounted() {
-    const myRatingComponent = document.querySelector('my-rating')
+    const myRatingComponent = this.$refs.myRating // document.querySelector('my-rating') // NOTE 打包后找不到
     setTimeout(() => {
       // myRatingComponent.value = 4;
       myRatingComponent.setAttribute('value', 4)
       console.log('set value')
-      this.maxValue = 10
       myRatingComponent.getValue({ maxValue: 10, value: 5 }).then(value => {
         console.log('get value', value)
       })
@@ -51,7 +75,7 @@ export default {
   },
   methods: {
     changeRating() {
-      this.value = Math.floor(Math.random() * this.maxValue)
+      this.innerValue = Math.floor(Math.random() * this.maxValue)
       this.persons.push({ age: 30 * Math.random() })
     },
     ratingChange({ detail }) {
@@ -61,4 +85,6 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+@import '../../../../node_modules/element-ui/lib/theme-chalk/button.css';
+</style>
