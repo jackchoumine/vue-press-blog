675f941866ee27251c33ee8ba0b4915f93ca6b63
jack <jackzhoumine@gmail.com>
2023-01-05 02:00:18 +0800

    feat: 不要解构store

--- packages/delightful-vue3-vite-project/src/components/PiniaDemos/CounterDemo.vue
+++ packages/delightful-vue3-vite-project/src/components/PiniaDemos/CounterDemo.vue
@@ -2,16 +2,25 @@
  * @Description : 
  * @Date        : 2023-01-05 01:34:31 +0800
  * @Author      : JackChou
- * @LastEditTime: 2023-01-05 01:43:32 +0800
+ * @LastEditTime: 2023-01-05 01:54:53 +0800
  * @LastEditors : JackChou
 -->
 <template>
-  <p>{{ counter.count }}</p>
+  <p>{{ count }}</p>
+  <button @click="add">+</button>
 </template>
 
 <script setup lang="ts">
 import { useCounter } from '@/store'
+import { storeToRefs } from 'pinia'
 const counter = useCounter()
+// NOTE 不要直接解构，会失去响应式
+// const { count } = counter
+const { count } = storeToRefs(counter)
+
+function add() {
+  ++counter.count
+}
 </script>
 
 <style scoped lang="scss"></style>
