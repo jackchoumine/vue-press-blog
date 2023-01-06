8a99e810c2bffacd4d9824657f214415da51dc4d
jack <jackzhoumine@gmail.com>
2023-01-06 23:06:12 +0800

    style: 调整代码顺序

--- packages/delightful-vue3-vite-project/src/views/PiniaPage.vue
+++ packages/delightful-vue3-vite-project/src/views/PiniaPage.vue
@@ -2,9 +2,13 @@
  * @Description : 
  * @Date        : 2023-01-05 01:33:43 +0800
  * @Author      : JackChou
- * @LastEditTime: 2023-01-06 23:02:38 +0800
+ * @LastEditTime: 2023-01-06 23:05:46 +0800
  * @LastEditors : JackChou
 -->
+<script setup lang="ts">
+import { CounterDemo, TodosDemo } from '@/components'
+</script>
+
 <template>
   <div class="page">
     <CounterDemo />
@@ -12,9 +16,5 @@
     <TodosDemo />
   </div>
 </template>
-
-<script setup lang="ts">
-import { CounterDemo, TodosDemo } from '@/components'
-</script>
 
 <style scoped lang="scss"></style>
