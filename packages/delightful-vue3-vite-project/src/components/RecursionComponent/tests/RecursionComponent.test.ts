b1c0b8494c47208908d34230e028cd2f24e5c07a
jack <jackzhoumine@gmail.com>
2023-01-07 21:36:01 +0800

    chore: 使用浅挂载

--- packages/delightful-vue3-vite-project/src/components/RecursionComponent/tests/RecursionComponent.test.ts
+++ packages/delightful-vue3-vite-project/src/components/RecursionComponent/tests/RecursionComponent.test.ts
@@ -2,18 +2,20 @@
  * @Description :
  * @Date        : 2023-01-07 20:20:28 +0800
  * @Author      : JackChou
- * @LastEditTime: 2023-01-07 20:59:23 +0800
+ * @LastEditTime: 2023-01-07 21:35:45 +0800
  * @LastEditors : JackChou
  */
 // import { expect, test } from 'vitest' // NOTE 全局导入 vitest 的函数
 import RecursionComponent from '../RecursionComponent.vue'
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 
 test('第一个测试', () => {
   expect(1 + 1).toBe(2)
 })
 
 test('RecursionComponent', () => {
-  const wrapper = mount(RecursionComponent)
+  const wrapper = shallowMount(RecursionComponent, {
+    $props: { list: ['JackChou', 'TomCat'] },
+  })
   console.log(wrapper.text())
 })
