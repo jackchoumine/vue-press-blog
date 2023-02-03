091a0c4da0d39d31c688b8ea826e6d91ec3988c6
ZhouQiJun <jackchou4job@163.com>
2023-02-03 12:39:53 +0800

    chore: 验证 el-input 的bug

---
+++ packages/delightful-vue3-vite-project/src/components/HookTest/UseDebounceRefDemo.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { MyInput } from '@/components'
+import { useDebounceRef } from '@/hooks'
+
+const input = useDebounceRef('', 2000)
+const myInput = useDebounceRef('', 2000)
+const elInput = useDebounceRef('', 2000)
+const qInput = useDebounceRef('', 2000)
+</script>
+
+<template>
+  <div class="use-debounce-ref">
+    <input type="text" v-model="input" />
+    <p>{{ input }}</p>
+    <MyInput v-model="myInput" />
+    <p>{{ myInput }}</p>
+    <h5>el-input</h5>
+    <ElInput v-model="elInput" />
+    <p>{{ elInput }}</p>
+    <h5>QInput</h5>
+    <QInput v-model="qInput" type="text" label="qInput" />
+    <p>{{ qInput }}</p>
+  </div>
+</template>
+
+<style lang="scss">
+.use-debounce-ref {
+  background-color: azure;
+}
+</style>
--- packages/tech-talk/README.md
+++ packages/tech-talk/README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Welcome to [Slidev](https://github.com/slidevjs/slidev)!
 
 To start the slide show:
@@ -9,3 +10,8 @@ To start the slide show:
 Edit the [slides.md](./slides.md) to see the changes.
 
 Learn more about Slidev on [documentations](https://sli.dev/).
+=======
+# tech-talk
+
+技术分享相关资料
+>>>>>>> 0f3d52299fca6116c37edd42ba1b8fd517813885
