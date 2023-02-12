9b4b610fa1e20480ab9bcf76f59819040f8dea54
jack <jackzhoumine@gmail.com>
2023-02-12 21:14:46 +0800

    声明类型

--- vue3/components/lib/index.ts
+++ vue3/components/lib/index.ts
@@ -2,7 +2,7 @@
  * @Description : 导出所有组件
  * @Date        : 2023-02-12 19:25:57 +0800
  * @Author      : JackChou
- * @LastEditTime: 2023-02-12 20:16:43 +0800
+ * @LastEditTime: 2023-02-12 21:13:58 +0800
  * @LastEditors : JackChou
  */
 import type { App } from 'vue'
@@ -18,3 +18,29 @@ export default (app: App) => {
   })
   return app
 }
+
+// export default {
+//  install(app: App, optipns = {}) {
+// NOTE vue3 插件可提供哪些功能？
+// 1.  注册全局组件
+// app.component('heead-demo',component)
+// 2. 注册全局指令
+// app.directive(key, dircetive)
+// 3. 添加全局自定义属性
+// FIXME 如何添加类型？
+// app.config.globalProperties.testFn = () => {
+//   console.log('install global properties')
+// }
+// app.config 可配置编译选项，原生tag/web component 全局属性
+// 4. 全局依赖注入
+// app.provide('key','注入变量')
+// 5. 提供全局混入
+// app.mixin({
+//   data() {
+//     return {
+//       mixinVar: '混入的变量',
+//     }
+//   },
+// })
+//   },
+// }
--- vue3/delightful-vue3-vite-project/src/hooks/useGlobalProps.ts
+++ vue3/delightful-vue3-vite-project/src/hooks/useGlobalProps.ts
@@ -1,8 +1,8 @@
 /*
  * @Author      : ZhouQiJun
  * @Date        : 2022-12-30 17:30:45
- * @LastEditors : ZhouQiJun
- * @LastEditTime: 2022-12-30 17:51:49
+ * @LastEditors : JackChou
+ * @LastEditTime: 2023-02-12 21:10:13 +0800
  * @Description : 导出全局属性
  */
 import type { ComponentInternalInstance } from 'vue'
@@ -11,7 +11,7 @@ import { getCurrentInstance } from 'vue'
 function useGlobalProps() {
   const { appContext } = getCurrentInstance() as ComponentInternalInstance
   const globalProps = appContext.config.globalProperties
-  return { ...globalProps }
+  return globalProps
 }
 
 export default useGlobalProps
