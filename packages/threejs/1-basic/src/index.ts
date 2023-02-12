052f09d7248d78c54fd6092873276a5ad09e976a
jack <jackzhoumine@gmail.com>
2023-02-12 23:58:48 +0800

    threejs 开发环境搭建

---
+++ packages/threejs/1-basic/src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description :
+ * @Date        : 2023-02-12 23:50:51 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2023-02-12 23:50:52 +0800
+ * @LastEditors : JackChou
+ */
+import * as Three from 'three'
+
+console.log(Three)
