86ea1e51145902d18294593009008c970fd77264
jack <jackzhoumine@gmail.com>
2023-02-18 18:48:19 +0800

    chore: 搭建vue2开发环境

---
+++ vue2/jack-ui-vue/.prettierrc.cjs
@@ -0,0 +1,29 @@
+/*
+ * @Description :
+ * @Date        : 2023-02-18 18:43:20 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2023-02-18 18:43:20 +0800
+ * @LastEditors : JackChou
+ */
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  printWidth: 90,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  // importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  // plugins: ['@trivago/prettier-plugin-sort-imports'],
+  // importOrder: [
+  //   '^@/(.*)$',
+  //   '^#c/(.*)$',
+  //   '^../(.*)',
+  //   '^./((?!scss).)*$',
+  //   '^./(.*)',
+  //   '^[./]',
+  // ],
+  // importOrderSeparation: true,
+  // importOrderSortSpecifiers: true,
+}
