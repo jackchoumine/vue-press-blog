91afa7981e2d5b9b2b4c9d05bde6adaf66a6970a
ZhouQiJun <jackchou4job@163.com>
2023-02-28 20:04:52 +0800

    style: 添加注释

--- vue3/es-ui/histoire.config.ts
+++ vue3/es-ui/histoire.config.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2023-02-20 09:02:00
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2023-02-23 20:21:17
+ * @Description :
+ */
 import { defineConfig } from 'histoire'
 import { HstVue } from '@histoire/plugin-vue'
 
