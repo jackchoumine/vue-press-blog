a2bc1060e284e0d3b0569ab15694be62aed3a462
ZhouQiJun <jackchou4job@163.com>
2023-03-15 11:03:02 +0800

    更新笔记

--- vue3/delightful-vue3-vite-project/src/components/HookTest/useCounter.ts
+++ vue3/delightful-vue3-vite-project/src/components/HookTest/useCounter.ts
@@ -1,9 +1,21 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2023-02-13 09:04:55
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2023-03-15 11:02:34
+ * @Description : Counter hook
+ */
 export default function useCounter(initCount: number = 0) {
   const count = ref(initCount)
   function add(step = 1) {
     count.value += step
   }
   function reduce(step = 1) {
+    // onMounted 不会执行
+    // onMounted(() => {
+    //   debugger
+    //   console.log('onMounted')
+    // })
     count.value -= step
   }
   return {
