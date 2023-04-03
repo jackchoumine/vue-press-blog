1fa823291d867703d7e00f817e1efa933f88253a
jack <jackzhoumine@gmail.com>
2023-04-03 22:36:30 +0800

    feat: 使用样式

--- vue3/quasar-project/src/pages/LayoutPage.vue
+++ vue3/quasar-project/src/pages/LayoutPage.vue
@@ -1,7 +1,21 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { biCameraReelsFill, biSignStopFill } from '@quasar/extras/bootstrap-icons'
+</script>
 
 <template>
-  <div class="bg-info">
-    <p class="text-center text-h3">quasar 布局</p>
+  <div>
+    <div class="bg-info">
+      <p class="text-center text-h3">quasar 布局</p>
+    </div>
+    <p class="text-center text-h3">icon 的使用</p>
+    <p>支持多种icon</p>
+    <p>bs 图标</p>
+    <q-icon name="font_download" color="primary" size="32px" />
+    <q-icon name="bi-activity" :color="'primary'" size="small" />
+    <q-icon name="bi-camera-reels-fill" :color="'primary'" size="lg" />
+    <br />
+    <p>使用 svg</p>
+    <q-icon :name="biCameraReelsFill" size="medium" style="color: var(--q-secondary)" />
+    <q-icon :name="biSignStopFill" size="2rem" style="color: var(--q-primary)" />
   </div>
 </template>
