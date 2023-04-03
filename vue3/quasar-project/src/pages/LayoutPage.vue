3efafb583922ecce024a8681bc35c7e143d4a2e8
jack <jackzhoumine@gmail.com>
2023-04-03 22:47:36 +0800

    feat: 图标

--- vue3/quasar-project/src/pages/LayoutPage.vue
+++ vue3/quasar-project/src/pages/LayoutPage.vue
@@ -10,12 +10,20 @@ import { biCameraReelsFill, biSignStopFill } from '@quasar/extras/bootstrap-icon
     <p class="text-center text-h3">icon 的使用</p>
     <p>支持多种icon</p>
     <p>bs 图标</p>
-    <q-icon name="font_download" color="primary" size="32px" />
-    <q-icon name="bi-activity" :color="'primary'" size="small" />
-    <q-icon name="bi-camera-reels-fill" :color="'primary'" size="lg" />
+    <q-icon name="font_download" color="primary" left size="32px" />
+    <q-icon name="bi-activity" :color="'primary'" left size="small" />
+    <q-icon name="bi-camera-reels-fill" :color="'primary'" left size="lg" />
     <br />
-    <p>使用 svg</p>
+    <p>svg图标</p>
     <q-icon :name="biCameraReelsFill" size="medium" style="color: var(--q-secondary)" />
-    <q-icon :name="biSignStopFill" size="2rem" style="color: var(--q-primary)" />
+    <q-icon :name="biSignStopFill" size="2rem" right style="color: var(--q-primary)" />
+    <br />
+    <div class="text-purple q-gutter-md">
+      <q-icon
+        v-for="size in ['xs', 'sm', 'md', 'lg', 'xl']"
+        :key="size"
+        :size="size"
+        name="font_download" />
+    </div>
   </div>
 </template>
