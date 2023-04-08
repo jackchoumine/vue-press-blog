7a8016d98df92667b71c0ced3c9bcdd04d4fc55b
jack <jackzhoumine@gmail.com>
2023-04-09 02:53:01 +0800

    feat: 事件

--- vue3/delightful-vue3-vite-project/src/components/LeafletDemos/InitMap.vue
+++ vue3/delightful-vue3-vite-project/src/components/LeafletDemos/InitMap.vue
@@ -2,22 +2,22 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-04-08 21:08:15
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-04-08 21:43:05
+ * @LastEditTime: 2023-04-09 02:51:39
  * @Description : 初始化地图
 -->
 <script lang="ts" setup>
 import L from 'leaflet'
 
 const mapContainer = ref()
-
 onMounted(() => {
-  initMap(mapContainer.value)
+  const map = initMap(mapContainer.value)
+  map.on('mousemove', onMouseMove)
 })
 
 function initMap(
   mapContainer: HTMLElement,
-  coordinates: [number, number] = [51.505, -0.09],
-  zoom: number = 13
+  coordinates: [number, number] = [26.55, 106.6],
+  zoom: number = 11
 ) {
   const map = L.map(mapContainer).setView(coordinates, zoom)
 
@@ -34,10 +34,31 @@ function initMap(
   // })
   return map
 }
+
+const coordinates = reactive({
+  lat: undefined,
+  lng: undefined,
+})
+function onMouseMove(e: L.LeafletMouseEvent) {
+  const { lat, lng } = e.latlng
+  coordinates.lat = lat.toFixed(4)
+  coordinates.lng = lng.toFixed(4)
+}
 </script>
 
 <template>
-  <div class="h-full" ref="mapContainer"></div>
+  <div class="h-full" ref="mapContainer">
+    <span class="coordinates">
+      纬度:{{ coordinates.lat }}，经度:{{ coordinates.lng }}
+    </span>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.coordinates {
+  position: absolute;
+  bottom: 10px;
+  left: calc(50%);
+  z-index: 401;
+}
+</style>
