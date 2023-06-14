9e0c2940f7c8a59df0e17fbc7a16fde24369a0b2
jack <jackzhoumine@gmail.com>
2023-06-15 02:38:49 +0800

    chore: 移动项目

--- vue2/my-admin/tests/unit/example.spec.js
+++ vue2/my-admin/tests/unit/example.spec.js
@@ -2,18 +2,19 @@
  * @Description: 可用性测试
  * @Date: 2021-06-04 21:49:39 +0800
  * @Author: JackChou
- * @LastEditTime: 2023-06-15 02:21:41
+ * @LastEditTime: 2023-06-15 02:37:25
  * @LastEditors: JackChou
  */
-// import { shallowMount } from '@vue/test-utils'
-// import HelloWorld from '@ex/HelloJest'
+import { shallowMount } from '@vue/test-utils'
+import HelloWorld from '@ex/HelloJest'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    // const wrapper = shallowMount(HelloWorld, {
-    //   propsData: { msg },
-    // })
-    expect(1 + 1).toEqual(2)
+    // console.log(HelloWorld)
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg },
+    })
+    expect(wrapper.text()).toMatch(msg)
   })
 })
