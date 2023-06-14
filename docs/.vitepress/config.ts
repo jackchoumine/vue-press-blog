6feaa82903a0749c10a076d693941ca93efc9747
jack <jackzhoumine@gmail.com>
2023-06-14 23:07:12 +0800

    docs: 更新博客配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,46 +2,58 @@
  * @Author      : ZhouQiJun
  * @Date        : 2023-06-12 03:01:09
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2023-06-14 22:04:58
- * @Description : 
+ * @LastEditTime: 2023-06-14 23:05:22
+ * @Description :
  */
 import { defineConfig } from 'vitepress'
-
+// import { default as replPlugin, } from 'vitepress-markdown-it-repl'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   ignoreDeadLinks: true,
-  title:`Jack Chou's blog`,
+  title: `Jack Chou's blog`,
   description: 'A VitePress Site',
   base: '/jack-front/',
   head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  markdown: {
+    // theme: 'material-theme-palenight',
+    lineNumbers: true,
+    // 中文锚点跳转，不添加
+    // anchor: {
+    //   slugify(str) {
+    //     return encodeURIComponent(str)
+    //   },
+    // },
+    toc: {
+      //生成目录的 markdown 标题 默认 2 3
+      // NOTE 需要安装 # ## ### 的层级编写才会在右侧生成目录
+      // NOTE 在文档里使用 [[toc]] 会在当前位置生成目录
+      level: [1, 2, 3, 4],
+    },
+  },
   themeConfig: {
     // logo:'https://avatars.githubusercontent.com/u/21340150?s=96&v=4',
-    logo:'/jack.png',
-    // https://vitepress.dev/reference/default-theme-config
-    // nav: [
-    //   { text: 'Home', link: '/' },
-    //   { text: 'Examples', link: '/markdown-examples' },
-    // ],
+    logo: '/jack.png',
+    // 搜索功能 需要自己去algolia注册账号
     search: {
       provider: 'algolia',
       options: {
         appId: '8J64VVRP8K',
         apiKey: 'a18e2f4cc5665f6602c5631fd868adfd',
-        indexName: 'vitepress'
-      }
+        indexName: 'vitepress',
+      },
     },
     nav: [
       { text: 'vue3', link: '/vue3/', activeMatch: '/vue3/' },
       { text: 'vue2', link: '/vue2/', activeMatch: '^/vue2/' },
-      { text: 'react', link: '/react/' },
-      { text: 'node', link: '/node/' },
-      { text: '科学', link: '/science/' },
+      { text: 'react', link: '/react/', activeMatch: '^/react/' },
+      { text: 'node', link: '/node/', activeMatch: '^/node/' },
+      { text: '科学', link: '/science/', activeMatch: '^/science/' },
       {
         text: 'web',
         items: [
-          { text: 'js', link: '/web/js/' },
+          { text: 'js', link: '/web/js/', activeMatch: '/web/' },
           // { text: 'css', link: '/web/css/' },
-          { text: 'webpack', link: '/webpack/' },
+          { text: 'webpack', link: '/webpack/', activeMatch: '/web/' },
         ],
       },
       {
@@ -57,30 +69,15 @@ export default defineConfig({
       },
       // { text: '面试', link: '/interview/' },
     ],
-    // markdown: {
-    //   lineNumbers: true, // 代码块显示行号
-    //   toc: {
-    //     includeLevel: [1, 2, 3, 4], //生成目录的 markdown 标题 默认 2 3
-    //   },
+    // docFooter: {
+    //   //上下篇文本
+    //   prev: '上一篇',
+    //   next: '下一篇',
     // },
-    // sidebar: [
-    //   {
-    //     text: 'Examples',
-    //     items: [
-    //       { text: 'Markdown Examples', link: '/markdown-examples' },
-    //       { text: 'Runtime API Examples', link: '/api-examples' },
-    //     ],
-    //   },
-    // ],
-    docFooter: { //上下篇文本
-      prev: '上一篇',
-      next: '下一篇'
-    },
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2022-present Jack Chou'
+      copyright: 'Copyright © 2022-present Jack Chou',
     },
-
-    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    // socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
   },
 })
--- docs/others/vitepress/index.md
+++ docs/others/vitepress/index.md
@@ -1,10 +1,12 @@
 # vitepress 使用技巧
 
-> 如何让 nav 保持高亮？
+[[toc]]
+
+## 如何让 nav 保持高亮？
 
 nav 里配置 `activeMatch`
 
-```js
+```js:no-line-numbers {3}
 {
   nav: [
     { text: 'vue3', link: '/vue3/', activeMatch: '/vue3/' },
@@ -12,3 +14,19 @@ nav 里配置 `activeMatch`
   ]
 }
 ```
+
+## 如何让代码块显示行号？
+
+```js
+{
+  markdown: {
+    lineNumbers: true,
+  },
+}
+```
+
+[更多 markdown 配置](https://vitepress.vuejs.org/guide/markdown.html#line-numbers)
+
+## 如何添加评论功能？
+
+<!-- TODO -->
